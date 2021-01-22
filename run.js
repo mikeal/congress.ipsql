a6fe93a11d1a7ada3631daa239c85b6636bc79f8
Mikeal Rogers <mikeal.rogers@gmail.com>
2021-01-22 04:55:44 +0000

    fix: run npx in quiet mode

--- run.js
+++ run.js
@@ -2,7 +2,7 @@
 import { execSync as exec } from 'child_process'
 
 const csv = `https://theunitedstates.io/congress-legislators/legislators-current.csv`
-const command = `npx ipsql@latest import export ${ csv } s3://ipsql-open-data`
+const command = `npx -q ipsql@latest import export ${ csv } s3://ipsql-open-data`
 
 const readme = `# Congress Terms (FiveThirtyEight) in IPSQL
 
