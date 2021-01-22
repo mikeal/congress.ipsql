b6292e91ad4884a3638f48a7d76ecb48e77f7e65
Mikeal Rogers <mikeal.rogers@gmail.com>
2021-01-22 04:43:05 +0000

    debug: bump version

--- run.js
+++ run.js
@@ -2,7 +2,7 @@
 import { execSync as exec } from 'child_process'
 
 const csv = `https://theunitedstates.io/congress-legislators/legislators-current.csv`
-const command = `npx ipsql@1.0.12 import export ${ csv } s3://ipsql-open-data`
+const command = `npx ipsql@1.0.13 import export ${ csv } s3://ipsql-open-data`
 
 const readme = `# Congress Terms (FiveThirtyEight) in IPSQL
 
