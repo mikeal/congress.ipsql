1592d1d8d8e590b2a7100bee52644fcc17ee34b9
Mikeal Rogers <mikeal.rogers@gmail.com>
2021-01-22 04:57:28 +0000

    fix: new data isn't from 538

--- run.js
+++ run.js
@@ -4,7 +4,7 @@ import { execSync as exec } from 'child_process'
 const csv = `https://theunitedstates.io/congress-legislators/legislators-current.csv`
 const command = `npx -q ipsql@latest import export ${ csv } s3://ipsql-open-data`
 
-const readme = `# Congress Terms (FiveThirtyEight) in IPSQL
+const readme = `# Congressional IPSQL Databases
 
 Build: ${ (new Date()).toLocaleString('en-US') }
 
